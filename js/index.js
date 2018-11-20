7a611291bd2ed8ab0ddc6d49f6476e3b17157650
Austin Markiewicz <austin@feith.com>
2018-11-20 15:34:57 -0500

    added view

--- js/index.js
+++ js/index.js
@@ -7,15 +7,35 @@ $(document).ready(function () {
     
     
     /* Inbox App Loading Icon */
-    $('#ibxBtn').click(function () {
+    $('#ibxBtn1').click(function () {
+        $('.ibx').removeClass('ibx-end').removeClass('ibx-run');
+        
+        $('#ibxBtn1, #ibxBtn2').attr('disabled', true);
+        
         $('.ibx').addClass('ibx-run');
+        setTimeout(function () {
+            $('.ibx').removeClass('ibx-end').removeClass('ibx-run');
+            
+            $('#ibxBtn1, #ibxBtn2').attr('disabled', false);
+            
+        }, 3000);
+    });
+    $('#ibxBtn2').click(function () {
+        $('.ibx').removeClass('ibx-end').removeClass('ibx-run');
+        $('.ibx').addClass('ibx-run');
+        
+        $('#ibxBtn1, #ibxBtn2').attr('disabled', true);
+        
         setTimeout(function () {
             $('.ibx').addClass('ibx-end');
             setTimeout(function () {
                 $('.ibx').removeClass('ibx-end').removeClass('ibx-run');
+                
+                $('#ibxBtn1, #ibxBtn2').attr('disabled', false);
             }, 4000);
         }, 3000);
     });
+    
     /* */
     
     
