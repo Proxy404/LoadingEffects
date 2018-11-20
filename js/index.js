52c5cb03dfd8598325902db75c8ef8423935655c
Austin Markiewicz <austin@feith.com>
2018-11-20 17:13:25 -0500

    updated font

--- js/index.js
+++ js/index.js
@@ -8,29 +8,32 @@ $(document).ready(function () {
     
     /* Inbox App Loading Icon */
     $('#ibxBtn1').click(function () {
-        $('.ibx').removeClass('ibx-end').removeClass('ibx-run');
+        var ibx = $(this).closest('.ce-effect-container').find('.ibx');
+        
+        $(ibx).removeClass('ibx-end').removeClass('ibx-run');
         
         $('#ibxBtn1, #ibxBtn2').attr('disabled', true);
         
-        $('.ibx').addClass('ibx-run');
+        $(ibx).addClass('ibx-run');
+        
         setTimeout(function () {
-            $('.ibx').removeClass('ibx-end').removeClass('ibx-run');
-            
+            $(ibx).removeClass('ibx-end').removeClass('ibx-run');
             $('#ibxBtn1, #ibxBtn2').attr('disabled', false);
-            
         }, 3000);
     });
     $('#ibxBtn2').click(function () {
-        $('.ibx').removeClass('ibx-end').removeClass('ibx-run');
-        $('.ibx').addClass('ibx-run');
+        var ibx = $(this).closest('.ce-effect-container').find('.ibx');
+        
+        $(ibx).removeClass('ibx-end').removeClass('ibx-run');
+        
+        $(ibx).addClass('ibx-run');
         
         $('#ibxBtn1, #ibxBtn2').attr('disabled', true);
         
         setTimeout(function () {
-            $('.ibx').addClass('ibx-end');
+            $(ibx).addClass('ibx-end');
             setTimeout(function () {
-                $('.ibx').removeClass('ibx-end').removeClass('ibx-run');
-                
+                $(ibx).removeClass('ibx-end').removeClass('ibx-run');
                 $('#ibxBtn1, #ibxBtn2').attr('disabled', false);
             }, 4000);
         }, 3000);
