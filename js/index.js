4c0f91ab2ac39f494277d4e9ab205e333b896dc3
glove <glove@feith.com>
2018-12-04 16:47:45 -0500

    Added peekaboo loader

--- js/index.js
+++ js/index.js
@@ -8,24 +8,24 @@ $(document).ready(function () {
 
     /* A1 */
     var whoosh = $('#whoosh')[0];
-    
+
     $('#a1Btn1').click(function () {
         var eff = $(this).closest('.ce-effect-container').find('.ce-effect');
-        
+
         $(eff).removeClass('a1-end').removeClass('a1-run');
-        
+
         $('#a1Btn1, #a1Btn2').attr('disabled', true);
-        
+
         $(eff).addClass('a1-run');
         whoosh.play();
-        
+
         setTimeout(function () {
             // $(eff).removeClass('a1-end').removeClass('a1-run');
             $('#a1Btn1, #a1Btn2').attr('disabled', false);
         }, 4000);
     });
-    
-    
+
+
     /* */
 
 
@@ -39,27 +39,27 @@ $(document).ready(function () {
         }, 4500);
     });
     /* */
-    
-    
-    
-    
+
+
+
+
     /* A2 */
     var wil = $('#wil')[0];
-    
+
     $('#a2Btn1').click(function () {
         var eff = $(this).closest('.ce-effect-container').find('.ce-effect');
-        
+
         $(eff).removeClass('a2-end').removeClass('a2-run');
-        
+
         $(eff).addClass('a2-run');
-        
+
         whoosh.play();
-        
+
         $('#a2Btn1').attr('disabled', true);
-        
+
         setTimeout(function () {
             $(eff).addClass('a2-end');
-            
+
             setTimeout(function () {
                 wil.play();
             }, 1800);
@@ -73,17 +73,17 @@ $(document).ready(function () {
 
 
     /* Inbox App Text Shadow / Color Transition */
-    
+
     var audio = $('#thump')[0];
-    
+
     $('#shadow_transition').click(function () {
-        
+
         $('#shadow_transition').attr('disabled', true);
-        
+
         setTimeout(function () {
             audio.play();
         }, 500);
-        
+
         $('.letters1').addClass('letters1-animate');
         $('.letters2').addClass('letters2-animate');
         setTimeout(function () {
@@ -92,7 +92,21 @@ $(document).ready(function () {
             $('.letters2').removeClass('letters2-animate');
         }, 3000);
     });
-    
+
+
+    /* G3 */
+
+    $('#g3').click(function () {
+
+        $('#g3').attr('disabled', true);
+
+        $('.cover').addClass('cover-animate');
+        setTimeout(function () {
+            $('#g3').attr('disabled', false);
+            $('.cover').removeClass('cover-animate');
+        }, 20000);
+    });
+
 
 
 });
